45929aa8f4655f577cb7a9d62fcd8bff7e06cbec
Ludyyy <d.ludwig18@web.de>
2020-06-03 14:51:14 +0200

    Projektdateien hinzufügen.

---
+++ WaiterQR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WaiterQR.Startup))]
+namespace WaiterQR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
