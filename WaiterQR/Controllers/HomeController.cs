0c1decadceefef66da9c1fdcd4f74168ab4f27bd
dkeles <dennis.keles@gmail.com>
2020-06-05 17:17:54 +0200

    Erster Wurf Itempresentation

--- WaiterQR/Controllers/HomeController.cs
+++ WaiterQR/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace WaiterQR.Controllers
 
             return View();
         }
+       
+        public ActionResult ItemPresentation()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
