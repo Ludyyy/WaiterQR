047e5f5d8f07c7d9dd7431985f0d0ea17e3de154
Ludyyy <d.ludwig18@web.de>
2020-06-11 15:44:02 +0200

    Writeindatabase

--- WaiterQR/Controllers/RestaurantController.cs
+++ WaiterQR/Controllers/RestaurantController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Web.Mvc;
+using WaiterQR.Database;
 using WaiterQR.Models;
 
 namespace WaiterQR.Controllers
@@ -24,6 +27,42 @@ namespace WaiterQR.Controllers
         [HttpPost]
         public ActionResult AddRestaurant(RestaurantModel restaurants)
         {
+            try
+            {
+                using (websitedbEntities db = new websitedbEntities())
+                {
+                    Restaurants restaurants1 = new Restaurants();
+                    restaurants1.Restaurant_Id = 3;
+                    restaurants1.Owner_LastName = "Ly";
+                    restaurants1.Owner_FirstName = "Kung muy";
+                    restaurants1.Restaurant_City = "Köln Kalk Ehrenmord";
+                    restaurants1.Restaurant_PostalCode = 51105;
+                    restaurants1.Restaurant_StreetName = "Rolshover Str. 12";
+                    
+                    db.Restaurants.Add(restaurants1);
+                    db.SaveChanges();
+                }
+
+                using (websitedbEntities db = new websitedbEntities())
+                {
+                    foreach(Restaurants r in db.Restaurants)
+                    {
+                       
+                    }
+
+                    Restaurants ds = db.Restaurants.Where(x => x.Restaurant_StreetName == "Rolshoverstr ").First();
+                    ds.Owner_FirstName = "Shengu Ling long";
+                    db.SaveChanges();
+                }
+                
+            catch (Exception e)
+            {
+                string s = string.Format("Fehler: {0}", e.Message);
+                s = string.Format("Typ: {0}", e.GetType());
+
+            }
+           
+
             try
             {
                 if (ModelState.IsValid)
